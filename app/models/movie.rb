c44a0e826fa82aca7ffed9f2d77786a1bc2adf52
kimielska <katie.imielska@chicagobooth.edu>
2022-05-06 15:55:13 +0000

    Added cast additionally

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -13,21 +13,32 @@
 #  director_id :integer
 #
 class Movie < ApplicationRecord
-
   def title_with_year
-      return self.title + "(" + self.year.to_s + ")"
-      #m = Movie.all.first
+    return self.title + "(" + self.year.to_s + ")"
+    #m = Movie.all.first
   end
 
   def director
-    d_id = self.director_id 
+    d_id = self.director_id
     matching_directors = Director.where({ :id => d_id })
-    the_director = matching_directors.at(0) 
+    the_director = matching_directors.at(0)
     return the_director
   end
 
-  def case 
-    #stretch that returns the actors assoc to one movie
-  end
+  def cast
+    #get list of characters
+    # for each char get actor
+    #save actors in list
+    #return list of actors
 
+    my_id = self.id
+    list_of_actors = Array.new
+    list_of_characters = Character.where({ :movie_id => my_id })
+    list_of_characters.each do |a_char|
+      c_actor_id = a_char.actor_id
+      actor = Actor.where({ :id => c_actor_id }).at(0)
+      list_of_actors.push(actor)
+    end
+    return list_of_actors
+  end
 end
